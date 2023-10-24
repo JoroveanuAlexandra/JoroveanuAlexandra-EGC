f56a5a6fcdb66c329450f6297552e6b4ff7b0def
Joroveanu Alexandra <alexandra.joroveanu@student.usv.ro>
2023-10-24 18:23:20 +0300

    Rezolvare tema 2

---
+++ Joroveanu/Program.cs
@@ -0,0 +1,98 @@
+﻿//Joroveanu Alexandra
+//3131A
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics.ES20;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Platform;
+
+using ClearBufferMask = OpenTK.Graphics.OpenGL.ClearBufferMask;
+using GL = OpenTK.Graphics.OpenGL.GL;
+using MatrixMode = OpenTK.Graphics.OpenGL.MatrixMode;
+using PrimitiveType = OpenTK.Graphics.OpenGL.PrimitiveType;
+namespace Joroveanu
+{
+    class SimpleWindow : GameWindow
+    {
+        private object gm;
+
+        // Constructor.
+        public SimpleWindow() : base(800, 600)
+        {
+            KeyDown += Keyboard_KeyDown;
+        }
+        // Tema 1- Exercitiul 2
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit();
+
+            if (e.Key == Key.F11)
+                if (this.WindowState == WindowState.Fullscreen)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Fullscreen;
+            //la apasarea butonului A imaginea se va deplasa cu 100 de pixeli spre stanga
+            if (e.Key == Key.A)
+                GL.Viewport(-100, 0, Width, Height);
+            //la apasarea butonului A imaginea se va deplasa cu 100 de pixeli spre dreapta
+            if (e.Key == Key.D)
+                GL.Viewport(100, 0, Width, Height);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.MidnightBlue);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            //tema 1- exercitiul 1
+            GL.MatrixMode(MatrixMode.Texture);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+           
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(-1.0f, 1.0f);
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(0.0f, -1.0f);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(1.0f, 1.0f);
+
+            GL.End();
+
+            this.SwapBuffers();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+
+            using (SimpleWindow example = new SimpleWindow())
+            {
+
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+}
